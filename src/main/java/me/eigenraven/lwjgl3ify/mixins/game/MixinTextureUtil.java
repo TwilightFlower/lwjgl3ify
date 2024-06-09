84d8d3f039d28e3bfecbce680c89a7bcab85def3
TwilightFlower <twilightflower@protonmail.com>
2024-06-09 14:51:20 -0400

    fix and re-enable stb image mixins

--- src/main/java/me/eigenraven/lwjgl3ify/mixins/game/MixinTextureUtil.java
+++ src/main/java/me/eigenraven/lwjgl3ify/mixins/game/MixinTextureUtil.java
@@ -4,24 +4,23 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-import net.minecraft.client.renderer.texture.TextureMap;
-
+import net.minecraft.client.renderer.texture.TextureUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import me.eigenraven.lwjgl3ify.textures.NativeBackedImage;
 
-@Mixin(TextureMap.class)
-public abstract class MixinTextureMap {
+@Mixin(TextureUtil.class)
+public abstract class MixinTextureUtil {
 
     @Redirect(
-        method = "loadTextureAtlas",
+        method = "readBufferedImage",
         at = @At(
             value = "INVOKE",
             target = "Ljavax/imageio/ImageIO;read(Ljava/io/InputStream;)Ljava/awt/image/BufferedImage;",
             remap = false))
-    private BufferedImage redirectImageRead(InputStream stream) {
+    private static BufferedImage redirectImageRead(InputStream stream) {
         try {
             return NativeBackedImage.make(stream);
         } catch (IOException ex) {
